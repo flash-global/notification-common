8684dfa392ea6c62fdce8181d3b7980472fe41c4
Boris <bce@opcoding.eu>
2017-09-20 16:53:50 +0200

    Fix websocket

--- src/Entity/Alert/WebSocket.php
+++ src/Entity/Alert/WebSocket.php
@@ -8,6 +8,30 @@ namespace Fei\Service\Notification\Entity\Alert;
  */
 class WebSocket extends AbstractAlert
 {
+    const ACTION_NEW = "NEW";
+    const ACTION_UPDATE = "UPDATE";
+
+    protected $action = self::ACTION_NEW;
+
+    /**
+     * @return string
+     */
+    public function getAction() : string
+    {
+        return $this->action;
+    }
+
+    /**
+     * @param string $action
+     * @return $this
+     */
+    public function setAction(string $action)
+    {
+        $this->action = $action;
+
+        return $this;
+    }
+
     /**
      * Get the type of the alert (email, sms, etc.)
      *
