c2535c9188fdc051966ed5b0ed45731a54d3e73e
LaV <l.venier@yoctu.com>
2018-10-26 14:37:39 -0400

    change platform

---
+++ src/Transformer/PlatformsTransformer.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * PlatformsTransformer.php
+ *
+ * @date        20/03/18
+ * @file        PlatformsTransformer.php
+ */
+
+namespace Fei\Service\Notification\Transformer;
+
+use Fei\Service\Notification\Entity\Platforms;
+use League\Fractal\TransformerAbstract;
+
+/**
+ * PlatformsTransformer
+ */
+class PlatformsTransformer extends TransformerAbstract
+{
+    /**
+     * @param Platforms $platforms
+     *
+     * @return array
+     */
+    public function transform(Platforms $platforms)
+    {
+        return [
+            'id' => $platforms->getId(),
+	    'type' => $platforms->getName(),
+	    'key' => $platforms->getKey(),
+	    'value' => $platforms->getValue(),
+	    'updatedat' => $platforms->getUpdatedAt(),
+	    'updatedby' => $platforms->getUpdatedBy()
+        ];
+    }
+}
